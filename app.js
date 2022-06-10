8df30a00c343db57ee97cd1a3af011cdf16a7818
FWaqas Ahmed <spire@localhost.localdomain>
2022-06-10 15:40:56 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const logg=require('./logger.js')
+
+console.log('Hi, this is my first app')
+
+logg.chk()
---
+++ logger.js
@@ -0,0 +1,6 @@
+function hello()
+{
+    console.log('His')
+}
+
+module.exports.chk=hello
